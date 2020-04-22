6f09fe62ec9b00d74a2dbe7e221b26006dca004c
YnotCode <40368753+YnotCode@users.noreply.github.com>
2020-04-21 21:04:46 -0400

    Updated server to host multiple html files
    New page had to be made for the "Terms and Conditions"

--- policiesServer.js
+++ policiesServer.js
@@ -1,10 +1,13 @@
 var app = require("express")();
 var server = require("http").Server(app);
 
-app.get("/", function(req, res){
+app.get("/privacy", function(req, res){
   res.sendFile(__dirname + "/privacyPolicy.html");
 });
 
+app.get("/terms", function(req, res){
+  res.sendFile(__dirname + "/termsConditions.html");
+});
 
 app.get("/:resource", function(req, res){
   var resource = req.params.resource;
